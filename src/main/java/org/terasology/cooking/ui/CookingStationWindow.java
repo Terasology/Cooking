d167387cd2fbdd48abe5be02fad2e14775c45fe6
DarkWeird <foxflameless@gmail.com>
2020-08-20 21:15:05 +0300

    fix: NUI extraction compatibility

--- src/main/java/org/terasology/cooking/ui/CookingStationWindow.java
+++ src/main/java/org/terasology/cooking/ui/CookingStationWindow.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.cooking.ui;
 
@@ -31,10 +18,10 @@ import org.terasology.processing.ui.VerticalTextureProgressWidget;
 import org.terasology.processing.ui.WorkstationScreenUtils;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.rendering.nui.BaseInteractionScreen;
-import org.terasology.rendering.nui.databinding.Binding;
-import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
+import org.terasology.nui.databinding.Binding;
+import org.terasology.nui.databinding.ReadOnlyBinding;
 import org.terasology.rendering.nui.layers.ingame.inventory.InventoryGrid;
-import org.terasology.rendering.nui.widgets.UILoadBar;
+import org.terasology.nui.widgets.UILoadBar;
 import org.terasology.workstation.component.WorkstationInventoryComponent;
 import org.terasology.workstation.component.WorkstationProcessingComponent;
 import org.terasology.workstationCrafting.ui.workstation.StationAvailableRecipesWidget;
